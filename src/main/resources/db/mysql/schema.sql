ab8ef4817594477bcb9741f52b01cf8357412943
YuriiDiduk <86839611+YuriiDiduk@users.noreply.github.com>
2022-05-23 23:07:28 +0300

    Update schema.sql

--- src/main/resources/db/mysql/schema.sql
+++ src/main/resources/db/mysql/schema.sql
@@ -1,4 +1,11 @@
-CREATE DATABASE petclinic;
+CREATE DATABASE IF NOT EXISTS petclinic;
+
+ALTER DATABASE petclinic
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
+
+GRANT ALL PRIVILEGES ON petclinic.* TO 'petclinic'@'%' IDENTIFIED BY 'petclinic';
+
 USE petclinic;
 
 CREATE TABLE IF NOT EXISTS vets (
